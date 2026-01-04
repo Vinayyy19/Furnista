042938319aae73c5cad41e7d40d51e9241d70447
Vinay Yadav <vinay.manoj.yadav491@gmail.com>
2026-01-04 22:08:46 +0530

    Db connection changed

--- db/db.js
+++ db/db.js
@@ -1,12 +1,21 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-function connectDB() {
-    mongoose.connect(process.env.MONGODB_URI)
-    .then(() => {
-        console.log('MongoDB connected successfully');
-    }).catch((err) => {
-        console.error('MongoDB connection error:', err);
+let isConnected = false;
+
+async function connectDB() {
+  if (isConnected) return;
+
+  try {
+    const db = await mongoose.connect(process.env.MONGODB_URI, {
+      bufferCommands: false,
     });
+
+    isConnected = db.connections[0].readyState;
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.error("MongoDB connection failed:", error);
+    throw error;
+  }
 }
 
 module.exports = connectDB;
