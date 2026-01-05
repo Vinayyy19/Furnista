a4bbe30899b52bf9aa2266f7034739f8d34837f1
Vinay Yadav <vinay.manoj.yadav491@gmail.com>
2026-01-05 13:23:14 +0530

    added cors Check

--- app.js
+++ app.js
@@ -9,38 +9,28 @@ const connectDB = require("./db/db");
 const userRoutes = require("./routes/user.routes");
 const productRoutes = require("./routes/product.routes");
 const adminRoutes = require("./routes/admin.routes");
-const cartRoutes = require('./routes/cart.routes');
-const orderRoutes = require('./routes/order.routes');
+const cartRoutes = require("./routes/cart.routes");
+const orderRoutes = require("./routes/order.routes");
 
 const app = express();
 
-(async () => {
-  try {
-    await connectDB();
-  } catch (err) {
-    process.exit(1);
-  }
-})();
-
+connectDB().catch(err => {
+  console.error("DB connection failed:", err.message);
+});
 
 app.use(
   cors({
     origin: [
       "https://furnistaa.vercel.app",
       "https://furnista.vercel.app",
-      "http://localhost:5173"
+      "http://localhost:5173",
     ],
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
 );
 
-
-
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
-
 app.use(cookieParser());
 
 app.get("/", (req, res) => {
@@ -50,7 +40,7 @@ app.get("/", (req, res) => {
 app.use("/users", userRoutes);
 app.use("/product", productRoutes);
 app.use("/admin", adminRoutes);
-app.use("/cart",cartRoutes);
-app.use("/orders",orderRoutes);
+app.use("/cart", cartRoutes);
+app.use("/orders", orderRoutes);
 
 module.exports = app;
